c649bf3bd5dc5da9b88aaf5b772f7425f2afb1a6
MT 中村 花菜 <hana.nakamura@metateam.co.jp>
2025-05-14 13:03:07 +0900

    JavaScript課題修正

---
+++ public/js/hello.js
@@ -0,0 +1,13 @@
+
+const nameInput = document.getElementById("nameInput");
+const saveButton = document.getElementById("saveButton");
+const message = document.getElementById("message");
+
+saveButton. addEventListener("click", function() {
+  const name = nameInput.value; //入力欄に書かれた文字を取ってきてnameに入れる
+  if (name === "") {
+    message.innerText = "※名前を入力してください！";
+  } else {
+    message.innerText = `こんにちは、${name}さん！`;
+  }
+});

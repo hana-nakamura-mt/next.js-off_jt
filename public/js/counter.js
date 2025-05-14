186fabfbd8b18348bd723d03ffe76e218ed0d807
MT 中村 花菜 <hana.nakamura@metateam.co.jp>
2025-05-14 13:16:41 +0900

    git上のインデントずれ対応

--- public/js/counter.js
+++ public/js/counter.js
@@ -1,19 +1,19 @@
 let countNumber = 0;
 
-  const plusButton = document.getElementById("plus");
-  plusButton.addEventListener("click", function(){
+	const plusButton = document.getElementById("plus");
+	plusButton.addEventListener("click", function(){
 		countNumber += 1;
 		document.getElementById("count").textContent = countNumber;
-  });
+	});
 
 	const minusButton = document.getElementById("minus");
-  minusButton.addEventListener("click", function(){
+	minusButton.addEventListener("click", function(){
 		countNumber -= 1;
 		document.getElementById("count").textContent = countNumber;
-  });
+	});
 
-  const resetButton = document.getElementById("reset");
-  resetButton.addEventListener("click", function(){
+	const resetButton = document.getElementById("reset");
+	resetButton.addEventListener("click", function(){
 		countNumber = 0;
 		document.getElementById("count").textContent = countNumber;
-  });
+	});

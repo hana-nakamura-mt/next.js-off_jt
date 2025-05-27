dc370dc9d46769fad031e101f20b71b63d68f107
MT 中村 花菜 <hana.nakamura@metateam.co.jp>
2025-05-27 12:55:37 +0900

    レイアウト構築演習課題

---
+++ src/app/menu/page.tsx
@@ -0,0 +1,17 @@
+export default function menu() {
+  return (
+    <div className="space-y-8">
+      <h1 className="text-3xl font-bold">メニュー</h1>
+      <ul>
+        <li>飲み物</li>
+        <li>フード</li>
+        <li>コーヒー豆</li>
+        <li>ティー・紅茶</li>
+        <li>タンブラー・マグカップ</li>
+        <li>グッズ</li>
+        <li>コーヒー器具</li>
+        <li>ギフト</li>
+      </ul>
+    </div>
+  );
+}
